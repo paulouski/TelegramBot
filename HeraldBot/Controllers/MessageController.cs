141c61b6b653f05fd2c537f5b389fe6aef8f44f2
pavlovski <adnels.ap@gmail.com>
2019-09-30 23:55:44 +0300

    Create base structure, add "hello" command

---
+++ HeraldBot/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using HeraldBot.Models;
+using HeraldBot.Models.Commands;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace HeraldBot.Controllers
+{
+    public class MessageController : ApiController
+    {
+        [Route(@"api/message/update")]
+        public async Task<OkResult> Update([FromBody]Update update)
+        {
+            IReadOnlyList<Command> commands = Bot.Commands;
+            Message message = update.Message;
+            TelegramBotClient client = await Bot.Get();
+
+            foreach (Command command in commands)
+            {
+                if (command.Contains(message.Text))
+                {
+                    command.Execute(message, client);
+                    break;
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ HeraldBot/Models/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace HeraldBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegrambotapp.azurewebsites.net:443/{0}";
+
+        public static string Name { get; set; } = "herald_chatbot";
+
+        public static string Key { get; set; } = "879113534:AAHzTMbIhdwtU4maetNL9bCv0np6LqT_V_8";
+    }
+}
